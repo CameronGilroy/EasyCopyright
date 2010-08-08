db6735587cebed3ac15fa560c87247d500bb8ea6
Cameron Gilroy <git@ripple7.com>
2010-08-08 19:20:53 +1000

    Some changes to make it a little bit more user friendly eg. more options in the snippet call

--- Copyright.php
+++ Copyright.php
@@ -5,21 +5,21 @@
   Based in part on Copyright by AMDbuilder
   Creator: Cameron Gilroy http://www.camerongilroy.com
   Support URL: http://www.camerongilroy.com/
-  
+
   Version History:
   1.0 - 30 July 2010 - Cameron Gilroy
 
   Usage:
   [[Copyright]] - Outputs "© [[Your Site name here]] 2010 - Powered by MODx"
   [[Copyright?StartYear=`2009`]] - Outputs "© [[Your Site name here]] 2009-2010 - Powered by MODx"
-  
+
   If the start year and current year are the same the output will be current year only.
 
 */
 
-$Name = "[[++site_name]]";
-$Powered = "Powered by <a href='http://www.modxcms.com'>MODx</a>";
-$Separator = "-";
+$Name = isset($Name)? $Name : "[[++site_name]]";
+$Powered = isset($Powered)? $Powered : "Powered by <a href='http://www.modxcms.com'>MODx</a>";
+$Separator = isset($Separator)? $Separator : "-";
 $cYear = date("Y");
 $StartYear = isset($StartYear)? $StartYear : $cYear;
 $years = ($cYear > $StartYear) ? $StartYear.$Separator.$cYear : $cYear;
