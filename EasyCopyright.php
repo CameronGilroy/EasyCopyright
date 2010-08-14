65f395c447e2e7e89bf7a2211f430dffdfc791af
Cameron Gilroy <git@ripple7.com>
2010-08-14 22:11:22 +1000

    Up Dated the name of the file

--- EasyCopyright.php
+++ EasyCopyright.php
@@ -1,6 +1,6 @@
 <?php
 /*
-  Copyright
+  Easy Copyright
   Version: 1.0
   Based in part on Copyright by AMDbuilder
   Creator: Cameron Gilroy http://www.camerongilroy.com
@@ -10,8 +10,8 @@
   1.0 - 30 July 2010 - Cameron Gilroy
 
   Usage:
-  [[Copyright]] - Outputs "© [[Your Site name here]] 2010 - Powered by MODx"
-  [[Copyright?StartYear=`2009`]] - Outputs "© [[Your Site name here]] 2009-2010 - Powered by MODx"
+  [[EasyCopyright]] - Outputs "© [[Your Site name here]] 2010 - Powered by MODx"
+  [[EasyCopyright?StartYear=`2009`]] - Outputs "© [[Your Site name here]] 2009-2010 - Powered by MODx"
 
   If the start year and current year are the same the output will be current year only.
 
