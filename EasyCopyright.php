8ffaf9bfcc2e9b03de6877c233e5966e4fdb62cc
Cameron Gilroy <git@ripple7.com>
2010-08-17 17:43:03 +1000

    Just inserted the readme into the head of the snippet

--- EasyCopyright.php
+++ EasyCopyright.php
@@ -1,19 +1,43 @@
 <?php
 /*
-  EasyCopyright
-  Version: 1.0
-  Based in part on Copyright by AMDbuilder
-  Creator: Cameron Gilroy http://www.camerongilroy.com
-  Support URL: http://www.camerongilroy.com/
-
-  Version History:
-  1.0 - 30 July 2010 - Cameron Gilroy
-
-  Usage:
-  [[EasyCopyright]] - Outputs "© [[Your Site name here]] 2010 - Powered by MODx"
-  [[EasyCopyright?StartYear=`2009`]] - Outputs "© [[Your Site name here]] 2009-2010 - Powered by MODx"
-
-  If the start year and current year are the same the output will be current year only.
+  h1. EasyCopyright
+  
+  h3. About
+  
+  * By Cameron Gilroy
+  * EasyCopyright
+  * Version: 1.0
+  * Based in part on Copyright by AMDbuilder
+  * Creator: Cameron Gilroy http://www.camerongilroy.com
+  * Support URL: http://www.camerongilroy.com/
+  * GitHub: http://github.com/camgill/EasyCopyright
+  
+  EasyCopyright is a simple snippet for ModX Revolution that inserts the the current year and the site name. Check the example below.
+  
+  ??© +CameronGilroy.com+ 2009-2010 - Powered by +MODx+??
+  
+  h3. Usage
+  
+  @[[EasyCopyright]]@ - Outputs ??© +[[Your Site name here]]+ 2010 - Powered by +MODx+??
+  @[[EasyCopyright? &StartYear=`2009`]]@ - Outputs ??© +[[Your Site name here]]+ 2009-2010 - Powered by +MODx+??
+  
+  
+  h2. Configuration
+  
+  The variables that are available are listed below with a description.
+  You can also call the contents of a chunk into any of these fields like this @[[EasyCopyright? &Powered=`[[$Your_Chunk_Name]]`]]@
+  
+  * Name - Your site name
+  * Start Year - What ever you put in!
+  * Powered - is the link to the ModX site
+  * Separator - is the separator that goes between the Start Year and the current year
+  
+  h3. Defaults
+  
+  * Name - Your site name
+  * Start Year - No Default must be called in the snippet
+  * Powered - is the link to the ModX site
+  * Separator - "~"
 
 */
 
